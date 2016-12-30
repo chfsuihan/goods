6ceb583d3907e7c8bed83811be1df6ac7311ddbb
chfsuihan <sycnthinking@163.com>
2016-12-30 10:13:25 +0800

    版本5 accept and apply

--- WebRoot/WEB-INF/sp_name_info_sjjr.sql
+++ WebRoot/WEB-INF/sp_name_info_sjjr.sql
@@ -59,10 +59,11 @@ on a.app_no = b.app_no
 into temp tmp_etps with no log;
 
 --查询申请案具体表
-select a.*,b.result
+select a.*,b.staff_name,b.user_id,b.result,b.text_opnn
  from tmp_etps_app a left join etpsname@qrypermitsoc:name_opinion b
-on a.app_no = b.app_no where b.result='受理'
+on a.app_no = b.app_no where b.result='受理' or b.result='不受理' or b.result='预审通过' or b.result = '预审不通过'
 into temp tmp_jc_apply with no log;
+
 
 --审核过的申请案
 select a.*,b.result,b.user_id,b.staff_name,b.text_opnn
@@ -164,6 +165,49 @@ on a.accept_organ = c.code left join temp_app_type d
 on a.app_case_type = d.type_id
 into temp tmp_etps_apply with no log;
 
+
+--jc_accept
+select app_no||'SHGSSH' as st_pid,
+	'0720'||'SHGSSH'||app_no as st_apply_id,
+	'SHGSSH' as st_src,
+	app_no as st_src_pid,
+	'0720' as st_item_id,
+	'企业名称预先核准登记' as st_item_name,
+	'SHGSSH' as st_org_id,
+	'上海市工商行政管理局' as st_org_name,
+	accept_organ as st_dept_name,
+	check_name as st_pro_name,
+	check_date as dt_do_time,
+	staff_name as st_person_name,
+	user_id as st_person_no,
+	'' as st_person_duty,
+	result as st_result,
+	text_opnn as st_opinion,
+	'' as st_days_type, 
+	'' as nm_commitment_days,
+	'' as nm_real_days,
+	check_name as st_applicant_name,
+	'法人' as ST_APPLICANT_TYPE,
+	'' as ST_CONTACT,
+	'' as ST_CONTACT_PHONE,
+	'' as ST_CONTACT_MOBILE,
+	'' as ST_CONTACT_EMAIL,
+	'窗口提交' as  ST_APPLY_METHOD,
+	'' as ST_APPLY_CONTENT,
+	'' as dt_intime,--不详
+	'' as st_ctct_prs_name,--不详
+	'' as st_ctct_prs_phone,--不详
+	'' as st_ctct_prs_mobile,--不详
+	'' as ST_WEBAPP_PASS,
+	accept_date as DT_CLCTDOCS_TIME,
+	'' as ST_APPLY_DOC_NO,--不详
+	'' as dt_end,--不详
+	'' as dt_begin,--不详
+	'' as ST_CONTACT_DOCU_TYPE,
+	'' as ST_CONTACT_DOCU_NO
+ from tmp_jc_apply a
+into temp tmp_etps_accept with no log;
+
 --审核表
 select app_no||'SHGSSH' as ST_PID,
 '0720'||'SHGSSH'||app_no as st_apply_id,
@@ -236,10 +280,17 @@ SELECT * FROM tmp_etps_apply;
 insert into JC_ACCEPT
 select  st_pid,st_apply_id,st_src,st_src_pid,st_item_id,st_item_name,
 st_org_id,st_org_name,st_dept_name,st_pro_name,dt_do_time,
-b.st_person_name,b.st_person_no,b.st_person_duty,'受理','同意',
-'','',1,'受理','',b.st_person_name,b.st_person_duty,'',st_src_pid as ST_ACCEPT_INFO_NO,'',
+b.st_person_name,b.st_person_no,b.st_person_duty,
+case when st_result in ('受理','预审通过') then '受理'
+	 else '不予受理' end,
+case when st_result in ('受理','预审通过') then '同意'
+	 else '不同意' end,
+'','',1,
+case when st_result in ('受理','预审通过') then '受理'
+	 else '不予受理' end,
+'',b.st_person_name,b.st_person_duty,'',st_src_pid as ST_ACCEPT_INFO_NO,'',
 '','','','','','','','','',''
-from tmp_etps_application b;
+from tmp_etps_accept b;
 
 --审核表
 insert into JC_AUDIT
